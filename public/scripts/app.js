37a0fee841f5d0ffc12641dded7f2e1df6ed050b
Brian Davidson <briandaviddavidson@yahoo.com>
2018-04-14 15:41:29 -0400

    Setting up babel 3-9

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,10 @@
-var template = <p>This is JSX from app.js!</p>;
+'use strict';
+
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
