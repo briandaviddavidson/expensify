693c734e91edf5342e940248cd6a50ac1f7a9c47
Brian Davidson <briandaviddavidson@yahoo.com>
2018-04-14 15:59:27 -0400

    Update app.js with challenge 1 3-10

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,54 @@
 'use strict';
 
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    'Indecision App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some info'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Test'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Test 2'
+    )
+  )
 );
+
+var template2 = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Brian Davidson'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 26'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: Washington, DC'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
-var template = <h1>Indecision App</h1>;
+var template = (
+<div>
+  <h1>Indecision App</h1>
+  <p>This is some info</p>
+  <ol>
+    <li>Test</li>
+    <li>Test 2</li>
+  </ol>
+</div>
+);
+
+var template2 = (
+<div>
+  <h1>Brian Davidson</h1>
+  <p>Age: 26</p>
+  <p>Location: Washington, DC</p>
+</div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
