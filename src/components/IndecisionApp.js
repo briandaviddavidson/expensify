0437316467c86bc8cfd692f4c54c71305ed3662d
Brian Davidson <briandaviddavidson@yahoo.com>
2018-05-03 22:30:11 -0400

    Add styling to list/widget 8-69

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -67,14 +67,16 @@ export default class IndecisionApp extends React.Component {
             hasOptions={this.state.options.length > 0}
             handlePick={this.handlePick}
           />
-          <Options
-            options={this.state.options}
-            handleDeleteOptions={this.handleDeleteOptions}
-            handleDeleteSingleOption={this.handleDeleteSingleOption}
-          />
-          <AddOption
-            handleAddOption={this.handleAddOption}
-          />
+          <div className='widget-body'>
+            <Options
+              options={this.state.options}
+              handleDeleteOptions={this.handleDeleteOptions}
+              handleDeleteSingleOption={this.handleDeleteSingleOption}
+            />
+            <AddOption
+              handleAddOption={this.handleAddOption}
+            />
+          </div>
         </div>
         <OptionModal selectedOption={this.state.selectedOption} clearSelectedOption={this.clearSelectedOption}/>
       </div>
