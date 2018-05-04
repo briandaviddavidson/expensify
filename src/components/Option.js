4e93136924a6c075c4f5372944faf6c95129e3e0
Brian Davidson <briandaviddavidson@yahoo.com>
2018-05-03 20:26:30 -0400

    Refactor stateless components to not use return

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-const Option = (props) => {
-  return (
-    <div>
-        {props.option}
-        <button onClick={(e) => {
-            props.handleDeleteSingleOption(props.option);
-        }}>
-          Remove
-        </button>
-    </div>
-  );
-}
+const Option = (props) => (
+  <div>
+      {props.option}
+      <button onClick={(e) => {
+          props.handleDeleteSingleOption(props.option);
+      }}>
+        Remove
+      </button>
+  </div>
+);
 
 export default Option;
