21bc995233fb9658489120de8033e86ce10e1e02
Brian Davidson <briandaviddavidson@yahoo.com>
2018-05-11 01:07:15 -0400

    Add selectors file based on work in section 10

---
+++ src/selectors/expenses.js
@@ -0,0 +1,22 @@
+// get visible expenses
+
+export default = (expenses, {text, sortBy, startDate, endDate}) => {
+  return expenses.filter((expense) => {
+    // must be past the start date
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    // must be before the end date
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    // Checks to see if the description includes any text in the description
+    const textMatch = expense.desc.toLowerCase().includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      // recent expenses at the top
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'amount') {
+      // most expensive at the top
+      return a.amount < b.amount ? 1 : -1;
+    }
+  });
+};
