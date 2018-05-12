1dc45245548bfdd33e361af932cbab097be79254
Brian Davidson <briandaviddavidson@yahoo.com>
2018-05-12 10:47:00 -0400

    Configured store and dispatched commands

--- src/app.js
+++ src/app.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import {ADD_EXPENSE} from './actions/expenses';
+import {SET_TEXT_FILTER} from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
+
+store.dispatch(ADD_EXPENSE({desc: 'water bill'}))
+store.dispatch(ADD_EXPENSE({desc: 'gas bill'}))
+store.dispatch(SET_TEXT_FILTER('water'));
+
+setTimeout(function () {
+  store.dispatch(SET_TEXT_FILTER('gas'));
+}, 3000)
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+
+console.log(visibleExpenses);
+
+const jsx = (
+<Provider store={store}>
+  <AppRouter />
+</Provider>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'));
