85ede98e4ebd9d7721dbc3e528c1db914d9da591
Brian Davidson <briandaviddavidson@yahoo.com>
2018-05-12 10:46:46 -0400

    Create expense list component

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
 
 const ExpenseDashboardPage = () => (
   <div>
-    this is my dashboard page
+    <ExpenseList />
   </div>
 );
 
