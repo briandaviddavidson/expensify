4833abd92149db27d2690c03656afb654f80d726
Brian Davidson <briandaviddavidson@yahoo.com>
2018-05-13 12:56:11 -0400

    Move remove button to each individual expense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {EDIT_EXPENSE} from '../actions/expenses';
+import {EDIT_EXPENSE, REMOVE_EXPENSE} from '../actions/expenses';
 import ExpenseForm from './ExpenseForm';
 
 const EditExpensePage = (props) => {
   return (
-    <ExpenseForm
-      expense={props.expense}
-      onSubmit={(expense) => {
-        props.dispatch(EDIT_EXPENSE(props.expense.id, expense));
+    <div>
+      <ExpenseForm
+        expense={props.expense}
+        onSubmit={(expense) => {
+          props.dispatch(EDIT_EXPENSE(props.expense.id, expense));
+          props.history.push('/');
+        }}
+      />
+      <button onClick={() => {
+        props.dispatch(REMOVE_EXPENSE({id: props.expense.id}));
         props.history.push('/');
-      }}
-    />
+      }}>Remove</button>
+    </div>
   );
 };
 
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {REMOVE_EXPENSE} from '../actions/expenses';
 
-const ExpenseListItem = ({dispatch, id, desc, amount, createdAt}) => (
+const ExpenseListItem = ({id, desc, amount, createdAt}) => (
   <div>
     <Link to={`/edit/${id}`}><h3>{desc}</h3></Link>
     <p>{amount} - {createdAt}</p>
-    <button onClick={() => {
-      dispatch(REMOVE_EXPENSE({id}))
-    }}>Remove</button>
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
