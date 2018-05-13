46b0bbd4b4324a45f54049492499493429d50a60
Brian Davidson <briandaviddavidson@yahoo.com>
2018-05-13 10:52:08 -0400

    Add sortby dropdown for date and amount

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {SET_TEXT_FILTER} from '../actions/filters';
+import {SET_TEXT_FILTER, SORT_BY_DATE, SORT_BY_AMOUNT} from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
   <div>
-    <input type="text" value={props.filters.text} onChange={(e) => {
-      props.dispatch(SET_TEXT_FILTER(e.target.value));
-    }}/>
+    <input
+      type="text"
+      value={props.filters.text}
+      onChange={(e) => {
+        props.dispatch(SET_TEXT_FILTER(e.target.value));
+      }}
+    />
+    <select
+      value={props.filters.sortBy}
+      onChange={(e) => {
+        if (e.target.value === 'date') {
+          props.dispatch(SORT_BY_DATE())
+        } else if (e.target.value === 'amount') {
+          props.dispatch(SORT_BY_AMOUNT())
+        }
+      }}
+    >
+      <option value='date'>Date</option>
+      <option value='amount'>Amount</option>
+    </select>
   </div>
 );
 
